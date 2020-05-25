de68a972709fbe5c35e8a8e2fa84b5dbe561e5e4
Tony Gustafsson <mail@tonyg.se>
2020-05-25 13:29:21 +0200

    Add own dialog module

---
+++ src/js/components/dialog.js
@@ -0,0 +1,54 @@
+const showDialog = (e, dialogEl) => {
+    e.preventDefault();
+
+    const body = document.querySelector('body');
+    const backdrop = document.createElement('div');
+    backdrop.classList.add('dialog-backdrop');
+
+    backdrop.addEventListener('click', (e) => {
+        closeDialog(e, dialogEl);
+    });
+
+    body.appendChild(backdrop);
+    body.style.overflowY = 'hidden';
+
+    dialogEl.classList.add('show');
+};
+
+const closeDialog = (e, dialogEl) => {
+    e.preventDefault();
+
+    const body = document.querySelector('body');
+    const backdrop = document.querySelector('.dialog-backdrop');
+    body.removeChild(backdrop);
+    body.style.overflowY = 'scroll';
+
+    dialogEl.classList.remove('show');
+};
+
+const dialog = (options) => {
+    const { dialogElementId, dialogTriggerElementId } = options;
+
+    const dialogEl = document.getElementById(dialogElementId);
+    const triggerEl = document.getElementById(dialogTriggerElementId);
+
+    if (!dialogEl) {
+        // Dialog element did not exist
+        return;
+    }
+
+    const closeBtn = document.createElement('button');
+    closeBtn.innerHTML = '&times;';
+    closeBtn.classList.add('dialog-close-btn');
+    dialogEl.appendChild(closeBtn);
+
+    closeBtn.addEventListener('click', (e) => {
+        closeDialog(e, dialogEl);
+    });
+
+    triggerEl.addEventListener('click', (e) => {
+        showDialog(e, dialogEl);
+    });
+};
+
+export default dialog;
