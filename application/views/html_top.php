104e4e24ac7d5f090eaddafd1484df9240ad847f
Tony Gustafsson <mail@tonyg.se>
2020-06-01 15:44:26 +0200

    FINALLY remove jQuery!

--- application/views/html_top.php
+++ application/views/html_top.php
@@ -36,7 +36,6 @@
 		</script>
 	<?php endif; ?>
 	
-	<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
 	<script type="text/javascript" src="<?php echo base_url('assets/js/styles.js?202005171562')?>"></script>
 	<script type="text/javascript" src="<?php echo base_url('assets/js/main.js?202005171562')?>"></script>
 	
