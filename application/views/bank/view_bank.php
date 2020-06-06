cc0de68d5e7055d684ca206f37d9f97b123dd2ef
Tony Gustafsson <tony.gustafsson@starrepublic.com>
2020-06-06 14:30:13 +0200

    Use SVG icons in bank

--- application/views/bank/view_bank.php
+++ application/views/bank/view_bank.php
@@ -1,11 +1,24 @@
-<header title="<?=$game['town_human'] . ' ' . $game['place']?>">
-	<h2><?=$game['town_human'] . ' ' . $game['place']?></h2>
-	<img src="<?=base_url('assets/images/places/bank_' . $game['nation'] . '.jpg')?>" class="header" alt="The bank">
+<header
+	title="<?=$game['town_human'] . ' ' . $game['place']?>">
+	<h2><?=$game['town_human'] . ' ' . $game['place']?>
+	</h2>
+	<img src="<?=base_url('assets/images/places/bank_' . $game['nation'] . '.jpg')?>"
+		class="header" alt="The bank">
 </header>
 
 <section class="action-buttons">
-	<a class="ajaxHTML" title="Transfer money to and from your bank account" href="bank"><img src="<?=base_url('assets/images/icons/money_bank.png')?>" alt="Account" width="32" height="32">Account</a>
-	<a class="ajaxHTML" title="Take or pay back a loan" href="bank/loan"><img src="<?=base_url('assets/images/icons/money_bank_loan.png')?>" alt="Loans" width="32" height="32">Loans</a>
+	<a class="ajaxHTML" title="Transfer money to and from your bank account" href="bank">
+		<svg width="32" height="32" alt="Savings">
+			<use xlink:href="#savings"></use>
+		</svg>
+		Account
+	</a>
+	<a class="ajaxHTML" title="Take or pay back a loan" href="bank/loan">
+		<svg width="32" height="32" alt="Loan">
+			<use xlink:href="#loan"></use>
+		</svg>
+		Loans
+	</a>
 </section>
 
 <p>
@@ -19,21 +32,40 @@
 
 <p><strong>The bank will keep 5 percent of what you put in your account...</strong></p>
 
-<form method="post" class="ajaxJSON" id="transfer_form" action="<?=base_url('bank/account_post')?>">
-	<input type="hidden" name="current_money" id="current_money" value="<?=$game['doubloons']?>">
-	<input type="hidden" name="current_money_bank" id="current_money_bank" value="<?=$game['bank_account']?>">
+<form method="post" class="ajaxJSON" id="transfer_form"
+	action="<?=base_url('bank/account_post')?>">
+	<input type="hidden" name="current_money" id="current_money"
+		value="<?=$game['doubloons']?>">
+	<input type="hidden" name="current_money_bank" id="current_money_bank"
+		value="<?=$game['bank_account']?>">
 	<input type="hidden" name="transfer" id="transfer" value="0">
 
 	<fieldset>
-		<legend><img src="<?=base_url('assets/images/icons/bank.png')?>" alt="Bank Account" width="32" height="32"> Bank account</legend>
-		
+		<legend>
+			<svg width="32" height="32" alt="Savings">
+				<use xlink:href="#savings"></use>
+			</svg>
+			Bank account
+		</legend>
+
 		<div class="slider-container">
 			<div id="account-slider" class="slider"></div>
 
 			<table>
-				<tr><td>Transfer</td><td><span id="transfer_presenter">0</span> dbl</td></tr>
-				<tr><td>Doubloons (Cash)</td><td><span class="money_after"><?=$game['doubloons']?></span> dbl</td></tr>
-				<tr><td>Account</td><td><span class="account_after"><?=$game['bank_account']?></span> dbl</td></tr>
+				<tr>
+					<td>Transfer</td>
+					<td><span id="transfer_presenter">0</span> dbl</td>
+				</tr>
+				<tr>
+					<td>Doubloons (Cash)</td>
+					<td><span class="money_after"><?=$game['doubloons']?></span>
+						dbl</td>
+				</tr>
+				<tr>
+					<td>Account</td>
+					<td><span class="account_after"><?=$game['bank_account']?></span>
+						dbl</td>
+				</tr>
 			</table>
 		</div>
 	</fieldset>
