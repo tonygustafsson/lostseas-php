b66699564f4e74b5aa324d9338b91dee37570675
Tony Gustafsson <tony.gustafsson@starrepublic.com>
2020-06-09 16:58:38 +0200

    Re add tavern images

--- application/views/tavern/view_tavern.php
+++ application/views/tavern/view_tavern.php
@@ -1,9 +1,9 @@
-<header
+<header class="area-header"
 	title="<?=$game['town_human'] . ' ' . $game['place']?>">
-	<h2><?=$game['town_human'] . ' ' . $game['place']?>
+	<h2 class="area-header__heading"><?=$game['town_human'] . ' ' . $game['place']?>
 	</h2>
 	<img src="<?=base_url('assets/images/places/tavern_' . $game['nation'] . '.jpg')?>"
-		class="header">
+		class="area-header__img">
 </header>
 
 <p>
@@ -42,7 +42,7 @@
 <section class="action-buttons">
 	<a class="ajaxJSON largepic" title="Buy dinners for you and your crew members"
 		href="<?=base_url('tavern/buy_post/dinners')?>">
-		<img src="<?=base_url('assets/images/icons/tavern_dinner.jpg')?>"
+		<img src="<?=base_url('assets/images/tavern/tavern_dinner.jpg')?>"
 			title="Increases your crew members health by +25 and their mood by +3">
 		Dinners<br><?=floor($prices['tavern_dinners']['buy'] * ($game['crew_members'] + 1))?>
 		dbl
@@ -50,7 +50,7 @@
 
 	<a class="ajaxJSON largepic" title="Buy wenches for you and your crew members"
 		href="<?=base_url('tavern/buy_post/wenches')?>">
-		<img src="<?=base_url('assets/images/icons/tavern_wench.jpg')?>"
+		<img src="<?=base_url('assets/images/tavern/tavern_wench.jpg')?>"
 			title="Increases your crew members health by +10 and their mood by +5.">
 		Wenches<br><?=floor($prices['tavern_wenches']['buy'] * ($game['crew_members'] + 1))?>
 		dbl
@@ -58,7 +58,7 @@
 
 	<a class="ajaxJSON largepic" title="Buy wine for you and your crew members"
 		href="<?=base_url('tavern/buy_post/wine')?>">
-		<img src="<?=base_url('assets/images/icons/tavern_wine.jpg')?>"
+		<img src="<?=base_url('assets/images/tavern/tavern_wine.jpg')?>"
 			title="Increases your crew members mood by +7.">
 		Wine<br><?=floor($prices['tavern_wine']['buy'] * ($game['crew_members'] + 1))?>
 		dbl
@@ -66,7 +66,7 @@
 
 	<a class="ajaxJSON largepic" title="Buy rum for you and your crew members"
 		href="<?=base_url('tavern/buy_post/rum')?>">
-		<img src="<?=base_url('assets/images/icons/tavern_rum.jpg')?>"
+		<img src="<?=base_url('assets/images/tavern/tavern_rum.jpg')?>"
 			title="Increases your crew members mood by +10.">
 		Rum<br><?=floor($prices['tavern_rum']['buy'] * ($game['crew_members'] + 1))?>
 		dbl
