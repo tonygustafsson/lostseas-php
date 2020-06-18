9fa65c36f109dd240de2ec9275182efbec671809
Tony Gustafsson <mail@tonyg.se>
2020-06-18 18:55:27 +0200

    FIx weirdness on ocean map

--- application/views/ocean/ocean_map.php
+++ application/views/ocean/ocean_map.php
@@ -1,60 +1,4 @@
 <div class="ocean-map">
-    <p>
-        Click on the town on the map to visit it. Hover over the town to get the name and country.
-    </p>
-
-    <p class="ocean-map__mobile-info">
-        On mobile devices you swipe the map back and forth to be able to see the whole of the map.
-    </p>
-
-    <p>
-        <strong>English:</strong>
-        <a href="<?=base_url('harbor/charles_towne')?>"
-            class="ajaxHTML">Charles Towne</a>,
-        <a href="<?=base_url('harbor/barbados')?>"
-            class="ajaxHTML">Barbados</a>,
-        <a href="<?=base_url('harbor/port_royale')?>"
-            class="ajaxHTML">Port Royale</a>,
-        <a href="<?=base_url('harbor/belize')?>"
-            class="ajaxHTML">Belize</a>
-
-        <br />
-
-        <strong>French:</strong>
-        <a href="<?=base_url('harbor/tortuga')?>"
-            class="ajaxHTML">Tortuga</a>,
-        <a href="<?=base_url('harbor/leogane')?>"
-            class="ajaxHTML">Leogane</a>,
-        <a href="<?=base_url('harbor/martinique')?>"
-            class="ajaxHTML">Martinique</a>,
-        <a href="<?=base_url('harbor/biloxi')?>"
-            class="ajaxHTML">Biloxi</a>
-
-        <br />
-
-        <strong>Spanish:</strong>
-        <a href="<?=base_url('harbor/panama')?>"
-            class="ajaxHTML">Panama</a>,
-        <a href="<?=base_url('harbor/havana')?>"
-            class="ajaxHTML">Havana</a>,
-        <a href="<?=base_url('harbor/villa_hermosa')?>"
-            class="ajaxHTML">Villa Hermosa</a>,
-        <a href="<?=base_url('harbor/san_juan')?>"
-            class="ajaxHTML">San Juan</a>
-
-        <br />
-
-        <strong>Dutch:</strong>
-        <a href="<?=base_url('harbor/bonaire')?>"
-            class="ajaxHTML">Bonaire</a>,
-        <a href="<?=base_url('harbor/curacao')?>"
-            class="ajaxHTML">Curacao</a>,
-        <a href="<?=base_url('harbor/st_martin')?>"
-            class="ajaxHTML">St. Martin</a>,
-        <a href="<?=base_url('harbor/st_eustatius')?>"
-            class="ajaxHTML">St. Eustatius</a>
-    </p>
-
     <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
         viewBox="0 0 850 540" preserveAspectRatio="xMinYMin meet">
         <image width="850" height="540"
@@ -62,25 +6,25 @@
 
         <!-- English -->
         <a href="<?=base_url('harbor/charles_towne')?>"
-            class="js-town ajaxHTML">
+            class="js-town ocean-map__town-link ajaxHTML">
             <image width="20" height="20" x="388" y="47" alt="Charles Towne" rel="england"
                 xlink:href="<?=base_url('assets/images/map/town.svg')?>" />
         </a>
 
         <a href="<?=base_url('harbor/barbados')?>"
-            class="js-town ajaxHTML">
+            class="js-town ocean-map__town-link ajaxHTML">
             <image width="20" height="20" x="779" y="387" alt="Barbados" rel="england"
                 xlink:href="<?=base_url('assets/images/map/town.svg')?>" />
         </a>
 
         <a href="<?=base_url('harbor/port_royale')?>"
-            class="js-town ajaxHTML">
+            class="js-town ocean-map__town-link ajaxHTML">
             <image width="20" height="20" x="452" y="306" alt="Port Royale" rel="england"
                 xlink:href="<?=base_url('assets/images/map/town.svg')?>" />
         </a>
 
         <a href="<?=base_url('harbor/belize')?>"
-            class="js-town ajaxHTML">
+            class="js-town ocean-map__town-link ajaxHTML">
             <image width="20" height="20" x="207" y="303" alt="Belize" rel="england"
                 xlink:href="<?=base_url('assets/images/map/town.svg')?>" />
         </a>
@@ -88,25 +32,25 @@
         <!-- France -->
 
         <a href="<?=base_url('harbor/tortuga')?>"
-            class="js-town ajaxHTML">
+            class="js-town ocean-map__town-link ajaxHTML">
             <image width="20" height="20" x="598" y="262" alt="Tortuga" rel="france"
                 xlink:href="<?=base_url('assets/images/map/town.svg')?>" />
         </a>
 
         <a href="<?=base_url('harbor/legogane')?>"
-            class="js-town ajaxHTML">
+            class="js-town ocean-map__town-link ajaxHTML">
             <image width="20" height="20" x="537" y="300" alt="Leogane" rel="france"
                 xlink:href="<?=base_url('assets/images/map/town.svg')?>" />
         </a>
 
         <a href="<?=base_url('harbor/martinique')?>"
-            class="js-town ajaxHTML">
+            class="js-town ocean-map__town-link ajaxHTML">
             <image width="20" height="20" x="752" y="353" alt="Martinique" rel="france"
                 xlink:href="<?=base_url('assets/images/map/town.svg')?>" />
         </a>
 
         <a href="<?=base_url('harbor/biloxi')?>"
-            class="js-town ajaxHTML">
+            class="js-town ocean-map__town-link ajaxHTML">
             <image width="20" height="20" x="259" y="68" alt="Biloxi" rel="france"
                 xlink:href="<?=base_url('assets/images/map/town.svg')?>" />
         </a>
@@ -114,25 +58,25 @@
         <!-- Spain -->
 
         <a href="<?=base_url('harbor/panama')?>"
-            class="js-town ajaxHTML">
+            class="js-town ocean-map__town-link ajaxHTML">
             <image width="20" height="20" x="382" y="459" alt="Panama" rel="spain"
                 xlink:href="<?=base_url('assets/images/map/town.svg')?>" />
         </a>
 
         <a href="<?=base_url('harbor/havana')?>"
-            class="js-town ajaxHTML">
+            class="js-town ocean-map__town-link ajaxHTML">
             <image width="20" height="20" x="351" y="204" alt="Havana" rel="spain"
                 xlink:href="<?=base_url('assets/images/map/town.svg')?>" />
         </a>
 
         <a href="<?=base_url('harbor/villa_hermosa')?>"
-            class="js-town ajaxHTML">
+            class="js-town ocean-map__town-link ajaxHTML">
             <image width="20" height="20" x="133" y="303" alt="Villa Hermosa" rel="spain"
                 xlink:href="<?=base_url('assets/images/map/town.svg')?>" />
         </a>
 
         <a href="<?=base_url('harbor/san_juan')?>"
-            class="js-town ajaxHTML">
+            class="js-town ocean-map__town-link ajaxHTML">
             <image width="20" height="20" x="661" y="283" alt="San Juan" rel="spain"
                 xlink:href="<?=base_url('assets/images/map/town.svg')?>" />
         </a>
@@ -140,25 +84,25 @@
         <!-- Holland -->
 
         <a href="<?=base_url('harbor/bonaire')?>"
-            class="js-town ajaxHTML">
+            class="js-town ocean-map__town-link ajaxHTML">
             <image width="20" height="20" x="610" y="429" alt="Bonarie" rel="holland"
                 xlink:href="<?=base_url('assets/images/map/town.svg')?>" />
         </a>
 
         <a href="<?=base_url('harbor/curacao')?>"
-            class="js-town ajaxHTML">
+            class="js-town ocean-map__town-link ajaxHTML">
             <image width="20" height="20" x="558" y="405" alt="Curacao" rel="holland"
                 xlink:href="<?=base_url('assets/images/map/town.svg')?>" />
         </a>
 
         <a href="<?=base_url('harbor/st_martin')?>"
-            class="js-town ajaxHTML">
+            class="js-town ocean-map__town-link ajaxHTML">
             <image width="20" height="20" x="720" y="301" alt="St. Martin" rel="holland"
                 xlink:href="<?=base_url('assets/images/map/town.svg')?>" />
         </a>
 
         <a href="<?=base_url('harbor/st_eustatius')?>"
-            class="js-town ajaxHTML">
+            class="js-town ocean-map__town-link ajaxHTML">
             <image width="20" height="20" x="741" y="327" alt="St. Eustatius" rel="holland"
                 xlink:href="<?=base_url('assets/images/map/town.svg')?>" />
         </a>
--- application/views/ocean/view_ocean.php
+++ application/views/ocean/view_ocean.php
@@ -43,5 +43,61 @@
 	</p>
 	<?php endif; ?>
 
+	<p>
+		Click on the town on the map to visit it. Hover over the town to get the name and country.
+	</p>
+
+	<p class="mobile-only">
+		On mobile devices you swipe the map back and forth to be able to see the whole of the map.
+	</p>
+
+	<p>
+		<strong>English:</strong>
+		<a href="<?=base_url('harbor/charles_towne')?>"
+			class="ajaxHTML">Charles Towne</a>,
+		<a href="<?=base_url('harbor/barbados')?>"
+			class="ajaxHTML">Barbados</a>,
+		<a href="<?=base_url('harbor/port_royale')?>"
+			class="ajaxHTML">Port Royale</a>,
+		<a href="<?=base_url('harbor/belize')?>"
+			class="ajaxHTML">Belize</a>
+
+		<br />
+
+		<strong>French:</strong>
+		<a href="<?=base_url('harbor/tortuga')?>"
+			class="ajaxHTML">Tortuga</a>,
+		<a href="<?=base_url('harbor/leogane')?>"
+			class="ajaxHTML">Leogane</a>,
+		<a href="<?=base_url('harbor/martinique')?>"
+			class="ajaxHTML">Martinique</a>,
+		<a href="<?=base_url('harbor/biloxi')?>"
+			class="ajaxHTML">Biloxi</a>
+
+		<br />
+
+		<strong>Spanish:</strong>
+		<a href="<?=base_url('harbor/panama')?>"
+			class="ajaxHTML">Panama</a>,
+		<a href="<?=base_url('harbor/havana')?>"
+			class="ajaxHTML">Havana</a>,
+		<a href="<?=base_url('harbor/villa_hermosa')?>"
+			class="ajaxHTML">Villa Hermosa</a>,
+		<a href="<?=base_url('harbor/san_juan')?>"
+			class="ajaxHTML">San Juan</a>
+
+		<br />
+
+		<strong>Dutch:</strong>
+		<a href="<?=base_url('harbor/bonaire')?>"
+			class="ajaxHTML">Bonaire</a>,
+		<a href="<?=base_url('harbor/curacao')?>"
+			class="ajaxHTML">Curacao</a>,
+		<a href="<?=base_url('harbor/st_martin')?>"
+			class="ajaxHTML">St. Martin</a>,
+		<a href="<?=base_url('harbor/st_eustatius')?>"
+			class="ajaxHTML">St. Eustatius</a>
+	</p>
+
 	<?php include(__DIR__. '/ocean_map.php'); ?>
 </div>
