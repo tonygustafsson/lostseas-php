cbbe203a5b6bebc8488735342dcc1e6d1110f641
Tony Gustafsson <mail@tonyg.se>
2020-06-18 18:44:07 +0200

    A bit nicer sound controls

--- application/views/partials/sound_controls_dialog.php
+++ application/views/partials/sound_controls_dialog.php
@@ -8,14 +8,15 @@
                 $play_music_display_style = $user['music_play'] == 1 ? 'none' : 'inline-block';
         ?>
 
-        <a id="music_link_play" title="Play game music" class="js-music-toggle-state sound-control-button" href="#"
+        <a id="music_link_play" title="Play game music" class="js-music-toggle-state sound-control-button mr-1" href="#"
                 style="display: <?=$play_music_display_style?>">
                 <svg width="32" height="32" class="Play music">
                         <use xlink:href="#sound-play"></use>
                 </svg>
         </a>
 
-        <a id="music_link_pause" title="Pause game music" class="js-music-toggle-state sound-control-button" href="#"
+        <a id="music_link_pause" title="Pause game music" class="js-music-toggle-state sound-control-button mr-1"
+                href="#"
                 style="display: <?=$pause_music_display_style?>">
                 <svg width="32" height="32" class="Pause music">
                         <use xlink:href="#sound-pause"></use>
@@ -35,7 +36,14 @@
         <h4>Sound effects</h4>
 
         <span id="sound_effects">
-                On <input type="radio" name="sound_effects" value="1" <?=($user['sound_effects_play'] == 1) ? ' checked' : ''?>>
-                Off <input type="radio" name="sound_effects" value="0" <?=($user['sound_effects_play'] == 0) ? ' checked' : ''?>>
+                <label class="pb-0">
+                        <input type="radio" name="sound_effects" value="1" <?=($user['sound_effects_play'] == 1) ? ' checked' : ''?>>
+                        On
+                </label>
+
+                <label class="pt-0">
+                        <input type="radio" name="sound_effects" value="0" <?=($user['sound_effects_play'] == 0) ? ' checked' : ''?>>
+                        Off
+                </label>
         </span>
 </div>
