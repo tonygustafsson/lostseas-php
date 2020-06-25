2b7e96dbab036e422e92f4d3a929c04fb3228742
Tony Gustafsson <mail@tonyg.se>
2020-06-25 20:02:59 +0200

    New snackbar at bottom of page, with close button

--- src/js/components/snackbar.js
+++ src/js/components/snackbar.js
@@ -1,10 +1,53 @@
 const generateId = () => `snack-${new Date().getTime()}`;
-const autoRemoveTimeMs = 4000;
-const autoHideTimeMs = 3500;
+
+const animationSpeed = 150;
+const autoRemove = true;
+const autoHideTimeMs = 4000;
+const autoRemoveTimeMs = animationSpeed + autoHideTimeMs;
 
 let zIndex = 1000;
 
-const show = (msg) => {
+const show = (snack) => {
+    const snackHeight = snack.offsetHeight;
+
+    snack.animate([{ transform: `translateY(${snackHeight}px)` }, { transform: 'translateY(0px)' }], {
+        duration: animationSpeed,
+        easing: 'ease-out',
+        fill: 'forwards'
+    });
+};
+
+const hide = (snack) => {
+    const snackHeight = snack.offsetHeight;
+
+    snack.animate([{ transform: 'translateY(0px)' }, { transform: `translateY(${snackHeight}px)` }], {
+        duration: animationSpeed,
+        easing: 'ease-out',
+        fill: 'forwards'
+    });
+
+    setTimeout(() => {
+        document.body.removeChild(snack);
+    }, animationSpeed);
+};
+
+const createCloseBtn = (snack) => {
+    const linkEl = document.createElement('a');
+
+    const buttonEl = document.createElement('span');
+    buttonEl.classList.add('snackbar__item__close-btn');
+    buttonEl.innerHTML = '&times;';
+
+    linkEl.appendChild(buttonEl);
+
+    linkEl.addEventListener('click', () => {
+        hide(snack);
+    });
+
+    snack.appendChild(linkEl);
+};
+
+const create = (msg) => {
     zIndex++;
 
     msg.id = generateId();
@@ -13,49 +56,40 @@ const show = (msg) => {
         msg.level = 'info';
     }
 
-    let hover = false;
+    let hasHovered = false;
 
     const snack = document.createElement('div');
     snack.id = msg.id;
-    snack.classList.add('snackbar-item');
-    snack.classList.add(`snackbar-item--${msg.level}`);
+    snack.classList.add('snackbar__item');
+    snack.classList.add(`snackbar__item--${msg.level}`);
     snack.innerHTML = msg.text;
     snack.style.zIndex = zIndex;
 
-    snack.addEventListener('mouseover', () => {
-        hover = true;
-    });
+    createCloseBtn(snack);
 
-    snack.addEventListener('touchstart', () => {
-        hover = true;
-    });
+    if (autoRemove) {
+        snack.addEventListener('mouseover', () => {
+            hasHovered = true;
+        });
 
-    snack.addEventListener('mouseout', () => {
-        hover = false;
-        snack.classList.remove('snackbar-item--active');
-    });
+        snack.addEventListener('touchstart', () => {
+            hasHovered = true;
+        });
+    }
 
     document.body.prepend(snack);
 
-    setTimeout(() => {
-        snack.classList.add('snackbar-item--active');
-    }, 0);
+    show(snack);
 
-    setTimeout(() => {
-        if (hover) {
-            return;
-        }
+    if (autoRemove) {
+        setTimeout(() => {
+            if (hasHovered) {
+                return;
+            }
 
-        snack.classList.remove('snackbar-item--active');
-    }, autoHideTimeMs);
-
-    setTimeout(() => {
-        if (hover) {
-            return;
-        }
-
-        document.body.removeChild(snack);
-    }, autoRemoveTimeMs);
+            hide(snack);
+        }, autoHideTimeMs);
+    }
 };
 
-export default show;
+export default create;
