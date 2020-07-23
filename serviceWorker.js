d5401ad428792e2d658e2caf072abd0c2de6dd4c
Tony Gustafsson <mail@tonyg.se>
2020-07-23 16:17:43 +0200

    Add a offline page for service worker

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,21 +1,26 @@
-self.addEventListener('install', function (event) {
+const offlinePage = './offline.html';
+
+self.addEventListener('install', (event) => {
     event.waitUntil(
-        caches.open('sw-cache').then(function (cache) {
-            return cache.addAll([
-                '/assets/js/main.js',
-                '/assets/js/styles.js',
-                '/assets/images/design/background-ship.svg',
-                '/assets/images/design/back_sky.jpg',
-                '/assets/images/favicon.svg'
-            ]);
+        caches.open('sw-cache').then((cache) => {
+            return cache.addAll([offlinePage, './assets/images/logo.svg']);
         })
     );
 });
 
-self.addEventListener('fetch', function (event) {
+self.addEventListener('fetch', (event) => {
     event.respondWith(
-        caches.match(event.request).then(function (response) {
-            return response || fetch(event.request);
+        caches.match(event.request).then((response) => {
+            if (response) {
+                // Cached content
+                return response;
+            } else {
+                // Fetch from network
+                return fetch(event.request).catch((error) => {
+                    // Send offline page on error
+                    return caches.match(offlinePage);
+                });
+            }
         })
     );
 });
