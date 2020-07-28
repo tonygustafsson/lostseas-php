6d5603a39e05e78c7fc267ca0b278edf8855c221
Tony Gustafsson <mail@tonyg.se>
2020-07-28 15:21:12 +0200

    Ability to repair all ships at once

--- application/views/shipyard/view_repair.php
+++ application/views/shipyard/view_repair.php
@@ -35,27 +35,67 @@
 		You can repair your ships here. You will only see your damaged ships.
 	</p>
 
-	<div class="button-area">
-		<?php foreach ($ship as $this_ship): ?>
-		<?php if ($this_ship['health'] < 100): ?>
-		<a id="ship_<?=$this_ship['id']?>"
-			class="ajaxJSON button big-image"
-			data-prompt-heading="Repair this <?=$this_ship['type']?>?"
-			data-prompt-text="The ship health will be restored to 100%. It will cost you <?php echo(100 - $this_ship['health']) * $prices['ship_repair']['buy']?> dbl."
-			title="Repair this <?=$this_ship['type']?>. Damaged by <?php echo(100 - $this_ship['health'])?> %"
-			href="<?=base_url('shipyard/repair_ship/' . $this_ship['id'])?>">
-			<img
-				src="<?=base_url('assets/images/ships/' . $this_ship['type'] . '.jpg')?>">
-			<?=$this_ship['name']?><br>(<?php echo(100 - $this_ship['health']) * $prices['ship_repair']['buy']?>
-			dbl)
+	<?php if ($game['ship_health_lowest'] < 100): ?>
+	<p class="text-center">
+		<a href="<?=base_url('shipyard/repair_all')?>"
+			class="ajaxJSON button primary" data-prompt-heading="Repair all ships?"
+			data-prompt-text="This will restore all ships health too 100%. It will cost you <?=$viewdata['repair_all_cost']?> dbl.">
+			<svg width="32" height="32" alt="Buy">
+				<use xlink:href="#icon-wrench"></use>
+			</svg>
+			Repair all ships (<?=$viewdata['repair_all_cost']?> dbl)
 		</a>
-		<?php endif; ?>
-		<?php endforeach; ?>
+	</p>
+
+	<div class="table-responsive">
+		<table>
+			<tr>
+				<th width="80"></th>
+				<th>Type</th>
+				<th>Name</th>
+				<th>Health</th>
+				<th>Repair cost</th>
+				<th></th>
+			</tr>
+
+			<?php foreach ($ship as $this_ship): ?>
+			<?php if ($this_ship['health'] < 100): ?>
+			<tr id="ship_<?=$this_ship['id']?>">
+				<td>
+					<img width="64" height="64"
+						src="<?=base_url('assets/images/ships/' . $this_ship['type'] . '.jpg')?>">
+				</td>
+				<td>
+					<?=ucfirst($this_ship['type'])?>
+				</td>
+				<td>
+					<?=$this_ship['name']?>
+				</td>
+				<td>
+					<?=$this_ship['health']?>%
+				</td>
+				<td>
+					<?=(100 - $this_ship['health']) * $viewdata['prices']['ship_repair']['buy']?>
+					dbl
+				</td>
+				<td class="text-center">
+					<a class="ajaxJSON button small"
+						data-prompt-heading="Repair this <?=$this_ship['type']?>?"
+						data-prompt-text="The ship health will be restored to 100%. It will cost you <?php echo(100 - $this_ship['health']) * $viewdata['prices']['ship_repair']['buy']?> dbl."
+						title="Repair this <?=$this_ship['type']?>. Damaged by <?php echo(100 - $this_ship['health'])?> %"
+						href="<?=base_url('shipyard/repair_ship/' . $this_ship['id'])?>">
+						Repair
+					</a>
+				</td>
+			</tr>
+			<?php endif; ?>
+			<?php endforeach; ?>
+		</table>
 	</div>
 
-	<?php if ($game['ship_health_lowest'] > 99): ?>
+	<?php else: ?>
 	<div class="info">
-		<p>Your ships seems to be in perfect health!</p>
+		<p><em>Your ships seems to be in perfect health.</em></p>
 	</div>
 	<?php endif; ?>
 </div>
